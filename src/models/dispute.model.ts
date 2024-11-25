a31b8c77a075cae7727d15ab77a156d8dd24f075
Kashish <kashish.biserwal@protonmail.com>
2024-11-25 08:08:30 +0530

    disp done

--- src/models/dispute.model.ts
+++ src/models/dispute.model.ts
@@ -1,12 +1,19 @@
 import mongoose from "mongoose";
 
 const disputeModel = new mongoose.Schema({
-    sellerId: { type: mongoose.Schema.Types.ObjectId, ref: "Seller" },
-    orderId: { type: mongoose.Schema.Types.ObjectId, ref: "ClientBilling" },
-    awb: { type: String, required: true },
-    description: { type: String, required: true },
-    image: { type: String, required: false },
-    accepted: { type: Boolean, default: false }
+  sellerId: { type: mongoose.Schema.Types.ObjectId, ref: "Seller" },
+  orderId: { type: mongoose.Schema.Types.ObjectId, ref: "ClientBilling" },
+  awb: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: false },
+  accepted: { type: Boolean, default: false },
+  orderBoxHeight: { type: Number, required: false },
+  orderBoxWidth: { type: Number, required: false },
+  orderBoxLength: { type: Number, required: false },
+  orderSizeUnit: { type: String, required: false },
+  orderWeight: { type: Number, required: false },
+  orderWeightUnit: { type: String, required: false },
+  createdAt: { type: Date, default: Date.now },
 });
 
 const SellerDisputeModel = mongoose.model("Dispute", disputeModel);
